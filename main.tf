08a0245fca07f7168bdc1123c2874112223b1431
Marques Johansson <mjohansson@equinix.com>
2021-04-27 15:07:02 -0400

    fix how gcs_key is loaded from cwd
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- main.tf
+++ main.tf
@@ -11,8 +11,11 @@ resource "random_string" "ssh_unique" {
 locals {
   ssh_user               = "root"
   project_name_sanitized = replace(var.project_name, "/[ ]/", "_")
-  ssh_key_name           = format("%s-%s-key", local.project_name_sanitized, random_string.ssh_unique.result)
-  gcs_key_path           = coalesce(abspath(var.path_to_gcs_key), fileset(path.module, var.relative_path_to_gcs_key), fileset(path.cwd, var.gcs_key_name))
+
+  ssh_key_name = format("%s-%s-key", local.project_name_sanitized, random_string.ssh_unique.result)
+
+  gcs_keys_cwd = flatten([[fileset(path.cwd, var.gcs_key_name)], ""])
+  gcs_key_path = coalesce(abspath(var.path_to_gcs_key), path.module, var.relative_path_to_gcs_key, local.gcs_keys_cwd[0])
 }
 
 resource "metal_project" "new_project" {
